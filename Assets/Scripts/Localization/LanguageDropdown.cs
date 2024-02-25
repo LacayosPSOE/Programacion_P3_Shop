d1d302755db3364cd8f5213f88ac1133acc018a0
u1961605 <joeljoanlopez@gmail.com>
2024-02-25 21:06:18 +0100

    Localización Implementada

---
+++ Assets/Scripts/Localization/LanguageDropdown.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class LanguageDropdown : MonoBehaviour
+{
+    private TMP_Dropdown _dropdown;
+
+    private void Start()
+    {
+        _dropdown = GetComponent<TMP_Dropdown>();
+    }
+
+    public void OnDropdownChange()
+    {
+        if (_dropdown == null) _dropdown = GetComponent<TMP_Dropdown>();
+
+        var _option = _dropdown.options[_dropdown.value].text;
+        var _language = Enum.Parse<Language>(_option);
+        Localizer.SetLanguage(_language);
+    }
+}
---
+++ Assets/Scripts/Localization/LocalizedText.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LocalizedText : MonoBehaviour
+{
+    public string TextKey;
+    private TMP_Text _textValue;
+
+    void Start()
+    {
+        _textValue = GetComponent<TMP_Text>();
+        SetText();
+    }
+
+    private void OnEnable()
+    {
+        Localizer.OnLanguageChangeDelegate += OnLanguageChanged;
+    }
+
+    private void OnDisable()
+    {
+        Localizer.OnLanguageChangeDelegate -= OnLanguageChanged;
+    }
+
+    private void OnLanguageChanged()
+    {
+        SetText();
+    }
+
+    private void SetText()
+    {
+        _textValue.text = Localizer.GetText(TextKey);
+    }
+}
